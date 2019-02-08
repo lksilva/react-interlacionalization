02b18bc37352d13faea33a9da792ce8080d5c575
Lucas Silva <lucas.silva@avecbrasil.com.br>
2019-02-08 11:04:54 -0200

    Commit inicial

--- src/App.js
+++ src/App.js
@@ -1,28 +1,53 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useTranslation, withTranslation } from 'react-i18next';
 
+
+export const MyTransletedComponent = () => {
+  const { t } = useTranslation();
+
+  return (
+    <Fragment>
+      <p>{t('Welcome.To New Universe')}</p>
+      <p>{t('Welcome.To React')}</p>
+    </Fragment>
+  )
+}
+// 
 class App extends Component {
+  state = {
+    value: 'pt-BR'
+  }
+
+  handleChange = (event) => {
+    this.setState({value: event.target.value}, () => {
+      this.props.i18n.changeLanguage(this.state.value);
+      console.log('props ==>>', this.props.i18n);
+      console.log('vent.target.value ==>>', this.state.value);
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+
+          <MyTransletedComponent />
+
+          <label>
+            Escolha seu idioma:
+            <select value={this.state.value} onChange={this.handleChange}>
+              <option value="pt-BR">Português</option>
+              <option value="en-US">English</option>
+            </select>
+          </label>
+
         </header>
       </div>
     );
   }
 }
 
-export default App;
+export default withTranslation()(App);
---
+++ src/i18n.js
@@ -0,0 +1,45 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+import LanguageDetector from 'i18next-browser-languagedetector';
+// not like to use this?
+// have a look at the Quick start guide 
+// for passing in lng and translations on init
+
+i18n
+  // detect user language
+  // learn more: https://github.com/i18next/i18next-browser-languageDetector
+  .use(LanguageDetector)
+  // pass the i18n instance to react-i18next.
+  .use(initReactI18next)
+  // init i18next
+  // for all options read: https://www.i18next.com/overview/configuration-options
+  .init({
+    resources: {
+      en: {
+        translation: {
+          "Welcome": {
+            "To React":  "Welcome to React and react-i18next",
+            "To New Universe":  "Welcome to new Universe"
+          }
+        }
+      },
+      br: {
+        translation: {
+          "Welcome": {
+            "To React": "Bem vindo ao React and react-i18next",
+            "To New Universe":  "Bem vindo ao novo Universo"
+          }
+        }
+      }
+    },
+    fallbackLng: "br",
+    debug: true,
+
+    interpolation: {
+      escapeValue: false, // not needed for react as it escapes by default
+    }
+  });
+
+
+export default i18n;
